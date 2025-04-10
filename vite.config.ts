e748a369fe460ef3062c278d0292aa5305389d84
joh_dev <kaklsp291116@gmail.com>
2025-04-10 10:45:27 +0700

    fix: add deploy page

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import { defineConfig, loadEnv } from 'vite'
+// import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import svgr from "vite-plugin-svgr"
 import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
 
-  const env = loadEnv(mode, process.cwd(), '')
-
+  // const env = loadEnv(mode, process.cwd(), '')
+  console.log('mode', mode);
   return {
     plugins: [
       react(),
@@ -18,17 +19,6 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: { '@': path.resolve(__dirname, 'src') },
     },
-    server: {
-      host: '0.0.0.0',
-      port: parseInt(env.PORT) || 3000,
-    },
-    preview: {
-      port: parseInt(env.PORT) || 3000,
-    },
-    build: {
-      chunkSizeWarningLimit: 1000
-    },
-    // base: process.env.VITE_BASE_PATH || "/LDB-Programing/Pos-management"
-    base: '/LDB-Programing/Pos-management',
+    base: '/demo-pos-management',
   }
 })
